ed53ff6b045bdaff8db6cfa7906fb7b4f1ba5609
chandanyadav888 <chamdam888@gmail.com>
2024-02-27 07:18:59 +0545

    renew

--- secret_pixel.py
+++ secret_pixel.py
@@ -14,21 +14,6 @@ from cryptography.hazmat.backends import default_backend
 import zlib
 from getpass import getpass
 
-"""
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program.  If not, see <https://www.gnu.org/licenses/>.
-"""
-
 def encrypt_data(data, public_key):
     # Generate a random session key
     session_key = os.urandom(32)  # 32 bytes for 256-bit key
